1809531198bdbe2ebf7d3be5e70dbf6581b9f527
Zack L <CSCzephyr@gmail.com>
2019-04-09 01:40:56 -0400

    Add full form

--- index.php
+++ index.php
@@ -7,6 +7,7 @@
      * The Cupcakes index page
      */
 
+    // setting up the flavors array
     $flavors = array();
     $flavors['grasshopper'] = 'The Grasshopper';
     $flavors['maple'] = 'Whiskey Maple Bacon';
@@ -15,6 +16,39 @@
     $flavors['velvet'] = 'Red Velvet';
     $flavors['lemon'] = 'Lemon Drop';
     $flavors['tiramisu'] = 'Tiramisu';
+
+    // when the form is submitted
+    if($_SERVER['REQUEST_METHOD'] == 'POST')
+    {
+        $errors = [];
+
+        if(empty($_POST['name']))
+        {
+            $errors[] = "You forgot to enter your name";
+        }
+
+        // checking if none of the checkboxes were checked
+        $chosen = array();
+        foreach($flavors as $flavor)
+        {
+            if (isset($_POST["$flavor"]))
+            {
+                $chosen[] = $flavor;
+            }
+        }
+
+        if(count($chosen) == 0)
+        {
+            $errors[] = 'You need to have at least one flavor.';
+        }
+
+        // if there are no errors
+        if (empty($errors))
+        {
+            $name = $_POST['name'];
+            echo "<h2>Thank you, $name, for your order!</h2>";
+        }
+    }
 
 ?>
 
@@ -34,14 +68,19 @@
     <form action="index.php" method="post">
 
         <p>Your name:</p>
-        <input type="text" placeholder="Please enter your name.">
+        <input type="text" name='name' placeholder="Please enter your name."
+               value="<?php if(isset($_POST['name'])) echo $_POST['name']; ?>">
 
         <p>Cupcake Flavors:</p>
-        <ul>
-            <?php
+        <?php
+            foreach ($flavors as $flavor)
+            {
+                echo "<input type='checkbox' name='$flavor' value='$flavor'> $flavor<br>";
+            }
+        ?>
 
-            ?>
-        </ul>
+        <br>
+        <input type="submit">
 
     </form>
 

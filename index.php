711f949c43cba54e29c8bcc3ecc8d18299b61723
Zack L <CSCzephyr@gmail.com>
2019-04-09 01:52:35 -0400

    Add error handling and form submission

--- index.php
+++ index.php
@@ -17,6 +17,21 @@
     $flavors['lemon'] = 'Lemon Drop';
     $flavors['tiramisu'] = 'Tiramisu';
 
+    ?>
+
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Cupcakes</title>
+</head>
+<body>
+
+<?php
+
     // when the form is submitted
     if($_SERVER['REQUEST_METHOD'] == 'POST')
     {
@@ -46,22 +61,33 @@
         if (empty($errors))
         {
             $name = $_POST['name'];
+            $price = 3.5;
+            $total = count($chosen) * $price;
+
             echo "<h2>Thank you, $name, for your order!</h2>";
+            echo "<p>Order Summary:</p><ul>";
+
+            foreach ($chosen as $choice)
+            {
+                echo "<li>$choice</li>";
+            }
+            echo '</ul>';
+
+            echo "<p>Order Total: $$total</p>";
+
+            exit();
+        }
+        // if there are errors
+        else
+        {
+            foreach ($errors as $error)
+            {
+                echo "<p>$error</p>";
+            }
         }
     }
 
 ?>
-
-<!doctype html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport"
-          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Cupcakes</title>
-</head>
-<body>
 
     <h1>Cupcake Fundraiser</h1>
 
